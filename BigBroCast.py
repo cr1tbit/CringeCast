73245f7671bd9925f671ca906c2a677f8de4ed7e
CritBit <Jakub.Sadowski1@gmail.com>
2020-02-01 03:56:52 +0100

    adding parametrized language endpoint

--- BigBroCast.py
+++ BigBroCast.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 #please ~hack~ harden me :(
 
+@app.route('/')
+def speak_any_lang():
+    saying = request.args.get("s", "test")
+    lang = request.args.get("l","en")
+    os.system(f'shellscripts/speak.sh "{saying}" {lang}')
+    return f'OK:{saying}'
+
 @app.route('/say')
 def say():
     saying = request.args.get("s", "test")
     os.system(f'shellscripts/en_speak.sh {saying}')
-    #return f'Hello, {name}!'
     return f'OK:{saying}'
 
 @app.route('/mow')
 def mow():
     saying = request.args.get("s", "test")
     os.system(f'shellscripts/pl_speak.sh {saying}')
-    #return f'Hello, {name}!'
     return f'okej:{saying}'
 
 app.run(host='0.0.0.0',port="42069")
